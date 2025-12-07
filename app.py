8249358dd1bffe761d6af8221a47b32fde7ae858
Tpg2004 <107260134+Tpg2004@users.noreply.github.com>
2025-12-07 23:10:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ CSS_STYLE = """
 
 /* --- Main App Background (Dark Purple) --- */
 [data-testid="stAppViewContainer"] {
-    background: linear-gradient(180deg, #4F359B 0%, #2E1A47 100%); 
+    background: linear-gradient(180deg, #fadb9c 0%, #fadb9c 100%); 
     background-attachment: fixed;
     color: #E6E0FF; 
 }
